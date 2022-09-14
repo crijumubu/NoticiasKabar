1df123a6f5ce1ce5283e670597d458e3905f4507
crijumubu <cristian.munoz.2020@upb.edu.co>
2022-09-14 18:47:08 -0500

    Extraccion de noticias para el portal del nuevo siglo

--- news/news/spiders/newsSpider.py
+++ news/news/spiders/newsSpider.py
@@ -1,12 +1,11 @@
-from itertools import product
 import scrapy
 from urllib.parse import urljoin
 
 class newsSpider(scrapy.Spider):
     
     name = 'news'
-
-    start_urls = ['https://www.eltiempo.com/', 'https://www.portafolio.co/']
+    
+    start_urls = ['https://www.eltiempo.com/', 'https://www.portafolio.co/', 'https://www.elnuevosiglo.com.co/']
     
     def parse(self, response):
 
@@ -14,7 +13,7 @@ class newsSpider(scrapy.Spider):
 
         if (originalUrl == 'https://www.eltiempo.com/'):
 
-            for productsLink in response.css('a.title.page-link'):
+            for productsLink in response.xpath("//a[@class='title page-link']"):
 
                 url = urljoin(response.url, productsLink.attrib['href'])
                 
@@ -22,13 +21,22 @@ class newsSpider(scrapy.Spider):
 
         elif (originalUrl == 'https://www.portafolio.co/'):
 
-            for productsLink in response.css('a.page-link'):
+            for productsLink in response.xpath("//a[@class='page-link']"):
 
                 url = urljoin(response.url, productsLink.xpath('.//@href').get())
 
                 yield scrapy.Request(url, callback=self.parseElPortafolio)
+            
+        elif (originalUrl == 'https://www.elnuevosiglo.com.co/'):
 
+            for productsLink in response.xpath("//div['text-node']/a"):
 
+                url = urljoin(response.url, productsLink.xpath('.//@href').get())
+
+                if (originalUrl in url and not 'seccion' in url):
+                    
+                    yield scrapy.Request(url, callback=self.parseElNuevoSiglo)
+                
     def parseElTiempo(self, response):
 
         try:
@@ -99,6 +107,23 @@ class newsSpider(scrapy.Spider):
 
             yield scrapy.Request(callback=self.emptyYield)
     
+    def parseElNuevoSiglo(self, response):
+
+        try:
+
+            yield {
+                'Title' : response.xpath("//span[@class='field field--name-title field--type-string field--label-hidden']/text()").get(),
+                'Descripcion' : response.xpath("//div[@class='clearfix text-formatted field field--name-body field--type-text-with-summary field--label-hidden field__item']/p/strong/text()").get(),
+                'Category' : response.xpath("//div[@class='container']/h3/text()").get(),
+                'Image' : response.xpath("//div[@class='multimedia']/img/@src").get().replace('//',''),
+                'Url' : response.url,
+                'Fuente' : 'El nuevo siglo'
+            }
+
+        except:
+
+            yield scrapy.Request(callback=self.emptyYield)
+
     def emptyYield(self):
 
         return {
