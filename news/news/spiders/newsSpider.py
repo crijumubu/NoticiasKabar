56213481133fca7a34144023e43a2b23caecc4c1
crijumubu <cristian.munoz.2020@upb.edu.co>
2022-09-14 10:27:04 -0500

    Implementación del scraper para el portal de portafolio

--- news/news/spiders/newsSpider.py
+++ news/news/spiders/newsSpider.py
@@ -5,19 +5,29 @@ from urllib.parse import urljoin
 class newsSpider(scrapy.Spider):
     
     name = 'news'
-    start_urls = ['https://www.eltiempo.com/']
+
+    start_urls = ['https://www.eltiempo.com/', 'https://www.portafolio.co/']
     
     def parse(self, response):
 
         originalUrl = response.url
 
         if (originalUrl == 'https://www.eltiempo.com/'):
-            
+
             for productsLink in response.css('a.title.page-link'):
 
                 url = urljoin(response.url, productsLink.attrib['href'])
                 
                 yield scrapy.Request(url, callback=self.parseElTiempo)
+
+        elif (originalUrl == 'https://www.portafolio.co/'):
+
+            for productsLink in response.css('a.page-link'):
+
+                url = urljoin(response.url, productsLink.xpath('.//@href').get())
+
+                yield scrapy.Request(url, callback=self.parseElPortafolio)
+
 
     def parseElTiempo(self, response):
 
@@ -30,24 +40,73 @@ class newsSpider(scrapy.Spider):
                 if '/files' in image:
                     imgDir = image
 
+            title = response.xpath("//h1[@class='titulo ']/text()").get()
+            description = response.xpath("//h2[@class='article-epigraph ']/text()").get()
+
+            if (title == None):
+
+                title = response.xpath("//h1[@class='titulo']/text()").get()
+
+            if (description == None):
+
+                description = response.xpath("//h2[@class='article-epigraph']/text()").get()
 
             yield {
-                'Title' : response.css('h1.titulo::text').extract(),
-                'Descripcion' : response.css('h2.article-epigraph::text').extract(),
-                'Category' : response.css('span.span-following::text').get(),
+                'Title' : title,
+                'Descripcion' : description,
+                'Category' : response.xpath("//span[@class='span-following']/text()").get(),
                 'Image' : response.url + imgDir,
                 'Url' : response.url,
-                'Fuente' : 'EL tiempo'
+                'Fuente' : 'El tiempo'
             }
 
         except:
 
-            yield{
+            yield scrapy.Request(callback=self.emptyYield)
 
-                'Title' : '',
-                'Descripcion' : '',
-                'Category' : '',
-                'Image' : '',
-                'Url' : '',
-                'Fuente' : '',
+    def parseElPortafolio(self, response):
+
+        try:
+
+            imgDir = ''
+
+            for image in response.xpath('//img/@src').extract():
+
+                if '/files' in image and '.jpeg' in image:
+                    imgDir = image
+
+            title = response.xpath("//h1[@class='title tiemposBold8 titularUnderBlanco']/text()").get()
+            description = response.xpath("//p[@class='epigraph regular_lead1']/text()").get()
+
+            if (title == None):
+
+                title = response.xpath("//h1[@class='title tiemposBold8 titularUnderBlanco ']/text()").get()
+
+            if (description == None):
+
+                description = response.xpath("//p[@class='epigraph regular_lead1 ']/text()").get()
+
+            yield {
+                'Title' : title,
+                'Descripcion' : description,
+                'Category' : " ".join(response.xpath("//a[@class='logoSection']/text()").get().split()),
+                'Image' : response.url + imgDir,
+                'Url' : response.url,
+                'Fuente' : 'Portafolio'
             }
+
+        except:
+
+            yield scrapy.Request(callback=self.emptyYield)
+    
+    def emptyYield(self):
+
+        return {
+
+            'Title' : '',
+            'Descripcion' : '',
+            'Category' : '',
+            'Image' : '',
+            'Url' : '',
+            'Fuente' : '',
+        }
