2ff0576a6b59cdfc3c85f5eef3a5be2aa53831a2
sdbaronc <47455237+sdbaronc@users.noreply.github.com>
2022-09-19 08:35:33 -0500

    Estructura y server
    Creacion de la estructura de la pagina e iniciacion del servidor local del desarrollo

---
+++ AppKabarWeb/src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+// configuraciones
+//midlewares
+//rutas
+//Archivos Estaticos
+//iniciacion server
+app.listen(3000, () => { 
+    console.log('server en puerto 3000'); 
+});
