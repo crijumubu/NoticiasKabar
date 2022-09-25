555a6e3a8ae4b02c9ab0974485022b49b8e4be53
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2022-09-24 23:05:31 -0500

    Header
    Clases y funciones del header en React

---
+++ React/kabar/src/header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="search">
+        <form>
+          <SearchBar holdTxt={"¿Que desea buscar?"} />
+          <SubmitBtn />
+        </form>
+      </header>
+    );
+  }
+}
+
+function SearchBar({holdTxt}) {
+  return <input type={"search"} className="search-inp" placeholder={holdTxt}></input>;
+}
+
+function SubmitBtn(){
+    return(
+        <button type="submit" className="search-btn">
+            <i className="bi bi-search icon-search"></i>
+        </button>
+    )
+}
+
+export default Header;
--- React/kabar/src/index.js
+++ React/kabar/src/index.js
@@ -1,73 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Header from './header';
 
-
-class Square extends React.Component {
-  constructor(props) {
-    super(props); // ! Llamar siempre que se haga un constructor
-    this.state = { // * Estado del componente
-      value: null,
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"        onClick={() => this.setState({value: 'X'})}      > 
-        {this.state.value}      </button> // * Cambiar estado, es decir, React renderizarta de nuevo el componenete y sus hijos.
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i}/>;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
+root.render(<Header />);
