bb5bf22e9d6ba50309455223935ecac2fdbcb2a9
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2022-09-25 00:27:18 -0500

    Aside Bar commit inicial

---
+++ React/kabar/src/aside.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./aside.css";
+
+function AsideBar(){
+    return(
+        <aside>
+            <div>
+
+            
+            </div>
+        </aside>
+    )
+}
