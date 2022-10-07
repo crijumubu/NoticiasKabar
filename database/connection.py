7619d472c0997f8b2408a6f041a2ab69c5c8eea6
crijumubu <cristian.munoz.2020@upb.edu.co>
2022-10-07 11:31:11 -0500

    Conexión a la base de datos y manipulación de las noticias extraídas

---
+++ database/connection.py
@@ -0,0 +1,45 @@
+from matplotlib.collections import Collection
+from pymongo import MongoClient
+from multipledispatch import dispatch
+
+class mongo:
+
+    def __init__(self):
+
+        self.client = MongoClient('mongodb+srv://KabarDBA:KabarDBA312@cluster0.50a4eap.mongodb.net/retryWrites=true&w=majority')
+        self.database = self.client.get_database('test')
+        self.collection = self.database['modelnoticias']
+
+    def insert(self, collection, many):
+
+        if (many):
+
+            self.collection.insert_many(collection)
+
+        else:
+
+            self.collection.insert_one(collection)
+
+    def count(self):
+
+        return self.collection.count_documents({})
+
+    @dispatch()
+    def select(self):
+
+        return list(self.collection.find())
+
+    @dispatch(str)
+    def select(self, title):
+
+        return self.collection.find_one({'Title' : title})
+
+    @dispatch()
+    def delete(self):
+
+        self.collection.delete_many({})
+
+    @dispatch(str)
+    def delete(self, title):
+
+        self.collection.delete_one({'Title' : title})
---
+++ database/controller.py
@@ -0,0 +1,42 @@
+from connection import mongo
+import json
+
+class controller:
+
+    def __init__(self):
+        
+        self.mongo = mongo()
+        self.getJson()
+
+    def getJson(self):
+
+        path = './../news/news.json'
+
+        with open(path) as news:
+
+            self.newsCollection = json.load(news)
+    
+    def countElements(self):
+
+        return len(self.mongo.select())
+
+    def insert(self):
+
+        self.mongo.insert(self.newsCollection)
+
+    def insertWithoutRepeating(self):
+        
+        for news in self.newsCollection:
+
+            newsTitle = news['Title']
+
+            if (self.mongo.select(newsTitle) == None):
+
+                self.mongo.insert(news, False)
+
+    def clearDatabase(self):
+
+        self.mongo.delete()
+
+control = controller()
+control.insertWithoutRepeating()
